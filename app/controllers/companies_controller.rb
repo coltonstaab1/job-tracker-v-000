a7c3c600b7ea8a2e4be5897986e20f6d1bb22ced
coltonstaab1 <colton.staab@gmail.com>
2016-06-23 14:33:53 -0400

    updated company show method to render html or json

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,6 +4,11 @@ class CompaniesController < ApplicationController
   end
 
   def show
+    @company = Company.find(params[:id])
+    respond_to do |f|
+      f.html { render :show }
+      j.json { render json: @company }
+    end
   end
 
 end
