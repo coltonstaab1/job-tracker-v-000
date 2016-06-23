9e3d407288c427641063741d97b30eed4772a795
coltonstaab1 <colton.staab@gmail.com>
2016-06-23 17:14:20 -0400

    debugging jobs.js with learn instructor

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-  getJobsData();
+  var data = getJobsData();
 });
 
 function getJobsData() {
     $.get("http://localhost:3000/jobs.json", function(data) {
-        debugger;
-        var job_title = data[0]['title'];
-        $("div").append(job_title);  
+      return data;
   });
 }
