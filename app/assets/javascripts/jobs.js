98781fa3fe347924a3a56615e8d5ba875f09d942
coltonstaab1 <colton.staab@gmail.com>
2016-06-23 15:29:41 -0400

    Updated the getJobsData function in my jobs.js file

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   alert('document loaded!')
+  var data = getJobsData();
+  alert(data);
 });
 
 function getJobsData() {
-  
+  return $.ajax({url: 'http://localhost:300/jobs.json', dataType : "jsonp"});
 }
