abbc5b88428d80fd4fb272af621f70f036f21d84
coltonstaab1 <colton.staab@gmail.com>
2016-06-27 15:36:17 -0400

    Created submitForm function in jobs.js

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+ // var data = getJobsData();
+  $.ajax({
+    url: 'http://localhost:3000/jobs.json',
+    type: 'GET'
+  }).done(function(jobs){
+    addDataToRow(jobs);
+  });
+
+  submitForm();
+});
+
+var addDataToRow = function(data) {
+  for (var i = 0; i < data.length; i++) {
+    var job = data[i];
+    $('#jobsTable').append([
+        '<tr>',
+          '<td>',
+            '<a href="jobs/',
+              job.id,
+            '">',
+            job.title,
+            '</a>',
+          '</td>',
+          '<td>',
+            job.city,
+          '</td>',
+          '<td>',
+            job.state,
+          '</td>',
+          '<td>',
+            job.description,
+          '</td>',
+        '</tr>'
+      ].join(''));
+  }
+}
+
+var submitForm = function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+  })
+}
