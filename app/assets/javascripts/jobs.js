bcee226fe27f5b6e6b5948989a28c3c59facaac6
coltonstaab1 <colton.staab@gmail.com>
2016-06-27 14:36:33 -0400

    Debugging getJobsData with Learn instructor:

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
   var data = getJobsData();
+  addDataToRow(data);
 });
 
 function getJobsData() {
     $.get("http://localhost:3000/jobs.json", function(data) {
       return data;
+      debugger;
   });
+}
+
+function addDataToRow(data) {
+  for (var i = 0; i < data.length; i++) {
+    $('#jobsTable').append([
+        '<tr>',
+          '<td>',
+            'Colton',
+          '</td>',
+        '</tr>'
+      ].join(''));
+  }
 }
