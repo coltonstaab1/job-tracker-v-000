bb068110d59013fd0a1d60f4532b11f51bb490bb
coltonstaab1 <colton.staab@gmail.com>
2016-06-28 14:29:24 -0400

    Fixed typo in seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623160108) do
+ActiveRecord::Schema.define(version: 20160628182718) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160623160108) do
     t.integer  "company_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "salary"
   end
 
   create_table "users", force: :cascade do |t|
