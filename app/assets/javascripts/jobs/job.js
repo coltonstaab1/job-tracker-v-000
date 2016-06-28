c1542125a9d4fce74ebe07cf90081905d2f8ea3e
coltonstaab1 <colton.staab@gmail.com>
2016-06-28 13:54:32 -0400

    Updated the jobs show page so that way it changes the data displayed on the page once you update it via the form

--- app/assets/javascripts/jobs/job.js
+++ app/assets/javascripts/jobs/job.js
@@ -27,10 +27,20 @@ var editJob = function() {
 var submitForm = function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
+    var jobNumber = window.location.pathname.split('/').pop();
+    var url = '/jobs/' + jobNumber;
     var values = $(this).serialize();
-    var posting = $.post('/jobs/1', values);
+    var posting = $.post(url, values);
     posting.done(function(data) {
-      $('form').trigger('reset');
+      updatePage(data);
+      $('#edit-job').hide();
     });
   });
+}
+
+var updatePage = function(data) {
+  $("[id*=title]").html(data.title);
+  $("[id*=city]").html(data.city);
+  $("[id*=state]").html(data.state);
+  $("[id*=description]").html(data.description);
 }
