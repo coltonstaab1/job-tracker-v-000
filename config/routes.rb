25229109d39ae1c22e2f6f1731ae277eeb31470e
coltonstaab1 <colton.staab@gmail.com>
2016-06-28 15:16:35 -0400

    Got rid of user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'static#home'
   resources :jobs, only: [:index, :show, :create, :update]
   resources :companies, only: [:index, :show]
