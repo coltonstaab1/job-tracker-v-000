8b323bed22182954f8ba982648a4cec16df25e06
coltonstaab1 <colton.staab@gmail.com>
2016-06-28 16:16:00 -0400

    Removed javascript include tags from jobs views

--- db/schema.rb
+++ db/schema.rb
@@ -20,12 +20,6 @@ ActiveRecord::Schema.define(version: 20160628182718) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "industries", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "city"
