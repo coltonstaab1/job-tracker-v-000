38f9a545b9618a5186df4ea2675b3bf4f3bd4b9a
coltonstaab1 <colton.staab@gmail.com>
2016-06-27 21:46:30 -0400

    Updated companies controller to allow it to respond to json requests

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,11 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all
+    respond_to do |f|
+      f.html { render :index}
+      f.json { render json: @companies }
+    end
   end
 
   def show
