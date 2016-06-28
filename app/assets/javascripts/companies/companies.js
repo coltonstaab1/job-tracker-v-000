cd4faf26633cdcc02d7cb561ef3d3cb67e263fb6
coltonstaab1 <colton.staab@gmail.com>
2016-06-27 22:02:37 -0400

    Made jobs.js functional by having it add the the names of all the companies to the ul

--- app/assets/javascripts/companies/companies.js
+++ app/assets/javascripts/companies/companies.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
- alert("Javascript functional!");
+    $.ajax({
+    url: 'http://localhost:3000/companies.json',
+    type: 'GET'
+  }).done(function(companies){
+    addCompaniesToPage(companies);
+  });
 });
+
+function addCompaniesToPage(companies) {
+  companies.forEach( function(company) {
+    $('ul').append(
+    ['<li>',
+      '<a href="/companies/',
+      company.id,
+      '">',
+      company.name,
+      '</a>',
+    '</li>'].join(''));
+  })
+}
+
