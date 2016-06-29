63a4c37d337db54111175d286f9f1e01613c9d82
coltonstaab1 <colton.staab@gmail.com>
2016-06-28 22:57:17 -0400

    Added turbolinks to JS manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require jobs/jobs
 //= require jobs/job
 //= require companies/companies
 //= require_tree .
+//= require turbolinks
