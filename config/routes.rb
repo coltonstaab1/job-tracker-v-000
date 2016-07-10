3591c8c193db5d56a5486d3880d436ce56659fb3
coltonstaab1 <colton.staab@gmail.com>
2016-07-10 12:01:17 -0400

    Created company_job and company_jobs nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static#home'
   resources :jobs, only: [:index, :show, :create, :update]
-  resources :companies, only: [:index, :show]
+  resources :companies, only: [:index, :show] do
+    resources :jobs, only: [:index, :show, :create, :update]
+  end
 end
